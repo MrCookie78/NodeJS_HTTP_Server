a56ad439952437e4bd7cbaa47b584728976ea060
Micaël Dupuis <micaeldupuis@live.fr>
2022-05-02 16:53:02 +0200

    Début CRUD API, ajout de la route api/names

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const memoryDb = new Map(); // est global
+let id = 0; // doit être global
+memoryDb.set(id++, {nom: "Alice"}) // voici comment set une nouvelle entrée.
+memoryDb.set(id++, {nom: "Bob"})
+memoryDb.set(id++, {nom: "Charlie"})
+
 const server = http.createServer((req, res) => {
 	let data = '';
 
@@ -24,7 +30,7 @@ const server = http.createServer((req, res) => {
 			}
 		}
 
-		// Route de l'image
+		// Route de du dossier public
 		else if(req.url.match("/public/*")){
 			if(req.method === 'GET'){
 				file = req.url.split('/')[2].split('.');
@@ -37,6 +43,22 @@ const server = http.createServer((req, res) => {
 				res.writeHead(200, {'content-type' : contentType});
 				const image = fs.readFileSync(path.join(__dirname, 'public', file[1], file[0] + '.' + file[1]));
 				res.write(image);
+			}
+
+			// Route avec autres méthodes
+			else {
+				res.writeHead(405, {'content-type' : 'text/html'});
+				const file405 = fs.readFileSync(path.join(__dirname, 'public', 'pages', '405.html'), 'utf8');
+				res.write(file405);
+			}
+		}
+
+		// Route API
+		else if(req.url.match("/api/names")){
+			if(req.method === 'GET'){
+				res.writeHead(200, {'content-type' : 'application/json'});
+				jsonText = JSON.stringify(Array.from(memoryDb.entries()));
+				res.write(jsonText);
 			}
 
 			// Route avec autres méthodes
